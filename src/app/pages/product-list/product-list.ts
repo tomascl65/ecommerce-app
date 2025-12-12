6f63a81808e978cd47fa86f9db4d40ad79f3fa43
tomascl <tomascl2015@gmail.com>
2025-12-12 08:11:10 -0500

    formulario de búsqueda de producto

--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,11 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList {}
+export class ProductList {
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      searchTerm: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    });
+  }
+
+  get searchTermControl(): FormControl {
+    return this.searchForm.get('searchTerm') as FormControl;
+  }
+
+  onSearch(): void {
+    if (this.searchForm.valid) {
+      const searchTerm = this.searchForm.get('searchTerm')?.value;
+      console.log('Buscar productos:', searchTerm);
+      // Aquí se implementaría la lógica de búsqueda
+    }
+  }
+}
